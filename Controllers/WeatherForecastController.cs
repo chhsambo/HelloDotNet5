9e55b47a2396b5871fdf58fb7d13572584e408e8
Sambo Chhay <sambo@pipay.com>
2021-03-08 09:56:17 +0700

    first commit

---
+++ Controllers/WeatherForecastController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace HelloDotNet5.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private readonly ILogger<WeatherForecastController> _logger;
+        private readonly WeatherClient client;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger, WeatherClient client)
+        {
+            _logger = logger;
+            this.client = client;
+        }
+
+        [HttpGet]
+        [Route("{city}")]
+        public async Task<WeatherForecast> Get(string city)
+        {
+            var forecast = await client.GetCurrentWeatherAsync(city);
+            return new WeatherForecast {
+                Summary = forecast.weather[0].description,
+                TemperatureC = (int)forecast.main.temp,
+                Date = DateTimeOffset.FromUnixTimeMilliseconds(forecast.dt).DateTime
+            };
+        }
+    }
+}
---
+++ ServiceSettings.cs
@@ -0,0 +1,8 @@
+namespace HelloDotNet5
+{
+    public class ServiceSettings
+    {
+        public string OpenWeatherHost { get; set; }
+        public string ApiKey { get; set; }
+    }
+}
